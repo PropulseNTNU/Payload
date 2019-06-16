3af09c881e2aeeb26509aaed71ff75e9825d0569
mariusud <mariusudnaes@gmail.com>
2019-06-16 19:45:54 +0200

    added burn-in for gas

---
+++ src/sensory/burner.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import time
+import board
+from busio import I2C
+import adafruit_bme680
+
+# Create library object using our Bus I2C port
+i2c = I2C(board.SCL, board.SDA)
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
+
+# change this to match the location's pressure (hPa) at sea level
+bme680.sea_level_pressure = 1006.0
+file = open("/home/pi/payload/src/sensory/burning_data.txt","a")
+
+while True:
+    ts = time.time()
+    file.write(str(ts) + "\t")
+    #print(str(bme680.pressure) + "hPA \t" + str(bme680.altitude) "meter \t" + str(bme680.humidity) 
+    print("\nTemperature: %0.1f C" % bme680.temperature)
+    file.write("\nTemperature: %0.1f C" % bme680.temperature)
+    print("Gas: %d ohm" % bme680.gas)
+    file.write("Gas: %d ohm" % bme680.gas)
+    print("Humidity: %0.1f %%" % bme680.humidity)
+    file.write("Humidity: %0.1f %%" % bme680.humidity)
+    print("Pressure: %0.3f hPa" % bme680.pressure)
+    file.write("Pressure: %0.3f hPa" % bme680.pressure)
+    print("Altitude = %0.2f meters" % bme680.altitude)
+    file.write("Altitude = %0.2f meters" % bme680.altitude)
+    time.sleep(1)
+
+
+#>>> st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+#import datetime
